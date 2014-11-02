cccd2546e8ecc8e27d8ab1b06d00fe4b788e4a85
Boris Nagaev <bnagaev@gmail.com>
2014-11-02 03:50:29 +0300

    fix error

--- checkall.lua
+++ checkall.lua
@@ -37,7 +37,7 @@ checkall.set_result = function(stud, mnem, py, ok, report, fi)
         local p8rep = report
         if not p8st then
             report = 'Скрипт работает, но есть нарекания ' ..
-            'к оформлению кода (см. ниже по-английски):'
+            'к оформлению кода (см. ниже по-английски):' ..
             '\n\n' .. report
             add_to_report(stud, mnem, 'pep8', py, report)
         else
