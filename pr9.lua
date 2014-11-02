17d9c0101c597eb3677b11e50dfc9752d0e4a64b
Boris Nagaev <bnagaev@gmail.com>
2014-11-02 04:49:06 +0300

    pr9: all-lunch, menu

--- pr9.lua
+++ pr9.lua
@@ -13,9 +13,11 @@ local match_numbers = require('helpers').match_numbers
 local match_numbers_no_order =
     require('helpers').match_numbers_no_order
 local match_str = require('helpers').match_str
+local match_strs = require('helpers').match_strs
 local bool_wrapper = require('helpers').bool_wrapper
 local one_of = require('helpers').one_of
 local unPack = require('helpers').unPack
+local copy_list = require('helpers').copy_list
 
 local pr9 = {
 
@@ -88,6 +90,38 @@ function()
         match_numbers_no_order(
         director, booker,
         director_or_booker, director_and_booker))
+end}},
+
+{'all-lunch', {
+function()
+    local ll_size = rr(1, 5)
+    local ll = {}
+    for i = 1, ll_size do
+        local v = shortrand()
+        table.insert(ll, v)
+    end
+    local ll2 = copy_list(ll)
+    ll2[#ll2] = 'Vasya'
+    table.remove(ll2, 1)
+    table.insert(ll2, 'and me')
+    local ll_str = table.concat(ll, ', ')
+    local ll2_str = table.concat(ll2, ', ')
+    return ll_str, match_str(ll2_str)
+end}},
+
+{'menu', {
+function()
+    local ll_size = rr(1, 5)
+    local ll = {}
+    local ll2 = {}
+    for i = 1, ll_size do
+        local v = shortrand()
+        table.insert(ll, v)
+        table.insert(ll2, tostring(i))
+        table.insert(ll2, v)
+    end
+    local ll_str = table.concat(ll, ', ')
+    return ll_str, match_strs(unPack(ll2))
 end}},
 
 }
