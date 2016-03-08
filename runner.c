9c8bd814c210497a2852bc4a3f592436c3e459d7
Boris Nagaev <bnagaev@gmail.com>
2016-03-08 19:41:42 +0300

    fix vulnerability in runner.c
    Previous version sets effective user and group ID's to RUNNER,
    but doesn't change real and saved ID's. The sandboxed process
    can call setresuid to become CHECKER again.
    
    How to explore:
    
      #define _GNU_SOURCE
      #include <unistd.h>
      #include <stdio.h>
    
      int main() {
          uid_t t1, t2, t3;
          getresuid(&t1, &t2, &t3);
          printf("real=%d effective=%d saved=%d\n", t1, t2, t3);
          setresuid(1003, 1003, 1003);
          getresuid(&t1, &t2, &t3);
          printf("real=%d effective=%d saved=%d\n", t1, t2, t3);
          fopen("/tmp/foobar", "w");
          return 0;
      }

--- runner.c
+++ runner.c
@@ -13,24 +13,52 @@ int main(int argc, char** argv) {
     if (argc < 2) {
         return 1;
     }
-    char* script = argv[1];
-    //
+    // make sure the caller is CHECKER
     if (getuid() != CHECKER_USER) {
         return 1;
     }
     if (getgid() != CHECKER_GROUP) {
         return 1;
     }
-    if (setgid(RUNNER_GROUP) != 0) {
+    // switch all user ID's to RUNNER
+    if (setresgid(RUNNER_GROUP, RUNNER_GROUP, RUNNER_GROUP) != 0) {
         return 1;
     }
-    if (setuid(RUNNER_USER) != 0) {
+    if (setresuid(RUNNER_USER, RUNNER_USER, RUNNER_USER) != 0) {
         return 1;
     }
+    // make sure all user ID's are set to RUNNER
+    uid_t ruid, euid, suid;
+    if (getresuid(&ruid, &euid, &suid) != 0) {
+        return 1;
+    }
+    if (ruid != RUNNER_USER) {
+        return 1;
+    }
+    if (euid != RUNNER_USER) {
+        return 1;
+    }
+    if (suid != RUNNER_USER) {
+        return 1;
+    }
+    gid_t rgid, egid, sgid;
+    if (getresgid(&rgid, &egid, &sgid) != 0) {
+        return 1;
+    }
+    if (rgid != RUNNER_GROUP) {
+        return 1;
+    }
+    if (egid != RUNNER_GROUP) {
+        return 1;
+    }
+    if (sgid != RUNNER_GROUP) {
+        return 1;
+    }
+    // run target program
+    char* script = argv[1];
     int status = system(script);
     if (status != 0) {
         return 1;
     }
     return 0;
 }
-
